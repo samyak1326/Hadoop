e4dff32664ee6d49ff866af9534dff9dcef780d3
SAMYAK JAIN <54874476+samyak1326@users.noreply.github.com>
2021-04-10 12:41:25 +0530

    Add files via upload

---
+++ WordCountDriver.java
@@ -0,0 +1,45 @@
+package WordCount;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class WordCountDriver {
+	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException, URISyntaxException 
+	{
+		Configuration conf = new Configuration();
+		Job j = new Job();
+		j.setJobName("My First Job");
+		j.setJarByClass(WordCountDriver.class );
+		j.setMapperClass(WordCountMapper.class );
+		j.setReducerClass(WordCountReducer.class);
+		j.setOutputKeyClass(Text.class);
+		j.setOutputValueClass(IntWritable.class);
+		
+		FileInputFormat.addInputPath(j, new Path(args[0]));
+		FileOutputFormat.setOutputPath(j, new Path(args[1]));
+		
+		URI uri = new URI(args[1].toString());
+		
+		FileSystem fs =  FileSystem.get(uri, conf);
+		
+		boolean x = fs.delete(new Path(uri),true);
+		
+		
+		int y =  j.waitForCompletion(true) ? 0 : 1;
+		
+	}
+
+}
+
+
+
+
